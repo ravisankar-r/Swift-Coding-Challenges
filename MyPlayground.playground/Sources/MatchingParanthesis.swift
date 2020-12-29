d1fc2971b0ccd46bf8123c69cac462d5d65e0272
Ravi Sankar <4372447+ravisankar-r@users.noreply.github.com>
2020-12-29 13:24:29 +0530

    Updated comments

--- MyPlayground.playground/Sources/MatchingParanthesis.swift
+++ MyPlayground.playground/Sources/MatchingParanthesis.swift
@@ -1,24 +1,33 @@
 import Foundation
+import XCTest
 
 // Given an expression string exp, write a program to examine whether the pairs and the orders of “{“, “}”, “(“, “)”, “[“, “]” are correct in exp.
 
-func hasMatchingParanthesis(_ str: String) -> Bool {
-    
-    let matchingDict:[Character: Character] = [")": "(",
-                                               "}": "{",
-                                               "[":"]"]
-    var stack: [Character] = []
-    let openingBraces: Set<Character> = ["(", "{", "["]
-    str.forEach { char in
+
+/* Solution
+ ----------------------------------
+ The challenge here is to identify the correct data structure to use. This can be solved using a stack. We need to identifiy the characters in the string and everytime we come across an opening brace we will push it onto a stack. Whenever we come across a closing brace we check the the top element on the stack , if both matches we pop it from the stack. The string will have matching brace if the stack is empty in the end
+ */
+
+public class MatchingParanthesis {
+    public static func hasMatchingParanthesis(_ str: String) -> Bool {
         
-        if openingBraces.contains(char) {
-            stack.append(char)
-        } else if let last = stack.last,
-                  let matchingBrace = matchingDict[char],
-                  last == matchingBrace {
+        let matchingDict:[Character: Character] = [")": "(",
+                                                   "}": "{",
+                                                   "[":"]"]
+        var stack: [Character] = []
+        let openingBraces: Set<Character> = ["(", "{", "["]
+        str.forEach { char in
             
-            stack.popLast()
+            if openingBraces.contains(char) {
+                stack.append(char)
+            } else if let last = stack.last,
+                      let matchingBrace = matchingDict[char],
+                      last == matchingBrace {
+                
+                stack.popLast()
+            }
         }
+        return stack.isEmpty
     }
-    return stack.isEmpty
 }
