bd4c989e66416c8f592bdb200b8d03792dd2a596
Ravi Sankar <4372447+ravisankar-r@users.noreply.github.com>
2020-12-29 13:31:27 +0530

    Updated Comments

--- MyPlayground.playground/Sources/MatchingParanthesis.swift
+++ MyPlayground.playground/Sources/MatchingParanthesis.swift
@@ -6,16 +6,26 @@ import XCTest
 
 /* Solution
  ----------------------------------
- The challenge here is to identify the correct data structure to use. This can be solved using a stack. We need to identifiy the characters in the string and everytime we come across an opening brace we will push it onto a stack. Whenever we come across a closing brace we check the the top element on the stack , if both matches we pop it from the stack. The string will have matching brace if the stack is empty in the end
+ The challenge here is to identify the correct data structure to use. This can be
+ solved using a stack. We need to identifiy the characters in the string and
+ everytime we come across an opening brace we will push it onto a stack.
+ Whenever we come across a closing brace we check the the top element on the
+ stack , if both matches we pop it from the stack.
+ The string will have matching brace if the stack is empty in the end
+ We can use Swift array to as a stack here and make use of the popLast() method to
+ pop elements as in from a stack.
  */
 
 public class MatchingParanthesis {
     public static func hasMatchingParanthesis(_ str: String) -> Bool {
         
+        // Use this to find out matching opening and closing pairs
         let matchingDict:[Character: Character] = [")": "(",
                                                    "}": "{",
                                                    "[":"]"]
+        // This will be out stack
         var stack: [Character] = []
+        // Lets use this set to store opening paranthesis. Sets can be better than arrays in terms of performace while looking up elements.
         let openingBraces: Set<Character> = ["(", "{", "["]
         str.forEach { char in
             
